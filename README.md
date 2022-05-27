66712ad80e50a0b309015e92e1a5b91898dffd1e
Byron 2016 <bgva2005@yahoo.com>
2022-05-27 13:02:09 -0500

    feat: 003 IE add ViewModels/ViewModelBase class

--- README.md
+++ README.md
@@ -5,4 +5,7 @@
 		- Implementing INotifyDataErrorInfo (View Model Data Validation) - EASY WPF (.NET CORE)
 			- https://www.youtube.com/watch?v=JZth6r2UXLU&list=PLA8ZIAm2I03h8QW3JaGrDBf7cq7F-cT4O&index=8
 			
+		- Crear un nuevo proyecto	
+		- Cambiar nombre de MainWindow y moverla a carpeta Views
+		- Crear clase  ViewModels/ViewModelBase que implementa INotifyPropertyChanged
 		
---
+++ aah_ValidationMVVM/aah_ValidationMVVM/ViewModels/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace aah_ValidationMVVM.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged; //autogenerado
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
